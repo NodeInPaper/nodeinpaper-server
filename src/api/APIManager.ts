dd492aa55f6083cb35ba2fb44703e06c388ef839
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2024-09-30 17:13:46 +0300

    Update APIManager.ts

--- src/api/APIManager.ts
+++ src/api/APIManager.ts
@@ -7,22 +7,23 @@ export interface ExecuteResponse {
   data: any;
 }
 
-export type RefArgumentType = { $ref: string };
+export type RefType = { $type: "Reference", id: string, value: any, container?: string };
+
+export function singularExecute(connection: SocketConnection, path: InfiniteProxyPathKey[], isSync: boolean) {
+  return new Promise(async (resolve, reject) => {
+    const res = (await connection.sendAndWaitResponse("SingularExecute", {
+      path: path.slice(0, -1),
+      sync: isSync
+    })) as ExecuteResponse;
+    if (!res.ok) return reject(res.data);
+    resolve(res.data);
+  });
+}
 
 export class APIManager {
   constructor(public nip: NIPServer) { }
 
   async buildAPI(connection: SocketConnection) {
-    function singularExecute(path: InfiniteProxyPathKey[], isSync: boolean) {
-      return new Promise(async (resolve, reject) => {
-        const res = (await connection.sendAndWaitResponse("SingularExecute", {
-          path: path.slice(0, -1),
-          sync: isSync
-        })) as ExecuteResponse;
-        if (!res.ok) return reject(res.data);
-        resolve(res.data);
-      });
-    }
     return {
       singular: createInfinitePathProxy((path, ...args) => {
         const lastKey = path.at(-1);
@@ -31,13 +32,13 @@ export class APIManager {
           case "$execute":
           case "$exec": {
             if (typeof lastKey.args![0]?.sync === "boolean") {
-              return singularExecute(path, !!lastKey.args![0]?.sync);
+              return singularExecute(connection, path, !!lastKey.args![0]?.sync);
             }
-            return singularExecute(path, false);
+            return singularExecute(connection, path, false);
           }
           case "$executeSync":
           case "$execSync": {
-            return singularExecute(path, true);
+            return singularExecute(connection, path, true);
           }
         }
 
