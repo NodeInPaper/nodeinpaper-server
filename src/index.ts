bf03e524425bc6b0ed3f6879be109da806a779f6
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2024-10-07 11:00:45 +0300

    a

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { NIPServer, NIPServerConfig } from "./NIPServer";
+import { recursiveImport } from "./utils";
 
 export function createNIPServer(config: NIPServerConfig) {
     return new NIPServer(config);
 }
+
+export const Utils = {
+    recursiveImport
+};
